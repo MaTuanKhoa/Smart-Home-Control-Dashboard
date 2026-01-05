04234ca94f61f389b4aae90e4ac13368668f44a4
Ma Tuấn Khoa <makhoa05@gmail.com>
2026-01-05 09:24:19 +0700

    Add files via upload

---
+++ Progress 1 (Screen Layout)/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "project_MAUI")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "project_MAUI.Pages")]
