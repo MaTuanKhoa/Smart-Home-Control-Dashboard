ab0e1f5a72c14ead91ef12ce1e3f066bb0400e75
Ma Tuấn Khoa <makhoa05@gmail.com>
2026-01-05 11:13:28 +0700

    Delete Progress 1 (Screen Layout) directory

--- Progress 1 (Screen Layout)/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "project_MAUI")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "project_MAUI.Pages")]
